7f7c7b08c3a078982bfb1e9e85f1e022aafdfc37
Grace Eileen Bass <grace.eilee.bass@gmail.com>
2025-06-19 23:36:38 +0300

    Create and insert new DOM node and remove existing main element

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// Remove the <main id="main"> element from the DOM
+const main = document.getElementById("main");
+main.remove();
+
+// Create a new <div> and assign it to a variable
+const newHeader = document.createElement("h1");
+
+// Give the <h1> an id of 'victory'
+newHeader.id = "victory";
+
+// Set its text content to show your name is the champion
+newHeader.textContent = "Grace is the champion";
+
+// Append it to the body (so it appears on the page)
+document.body.append(newHeader);
